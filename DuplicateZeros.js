e73d3891c7d81a338e69f7d93c296078291be4a2
Qi Zhao <911379608@qq.com>
2021-05-12 22:58:57 +0800

    <add> duplicate-zeros

---
+++ DuplicateZeros.js
@@ -0,0 +1,22 @@
+// https://leetcode-cn.com/problems/duplicate-zeros/
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+ var duplicateZeros = function(arr) {
+    var maxsize = arr.length
+    var length = arr.length
+    var addNum=0
+    for(let i=0,j=0;i<length;i++){
+        if(arr[i]===0){
+            arr.splice(i,0,0)
+            length++
+            i++
+            addNum++
+        }
+    }
+    for(let i=0;i<addNum;i++){
+        arr.pop()
+    }
+    
+};
